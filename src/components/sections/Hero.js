e6a89b2c8b3fea54161b4eb41a11bba8c878fbed
Gustavo Luna <gustavolunalimon@gmail.com>
2021-06-03 15:06:43 -0500

    [mod] add font & favicon

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -7,8 +7,8 @@ const Hero = () => {
             <section className="hero" style={{background: `#FF515D url(${ImgBackground}) center`}}>
                 <div className="container">
                     <div className="cont-info">
-                        <h1>A modern publishing platform</h1>
-                        <p></p>
+                        <h1><strong>A modern publishing platform</strong></h1>
+                        <p>Grow your audience and build your online brand</p>
                         <div className="botones">
                             <button className="boton secundario">Start for Free</button>
                             <button className="boton">Learn More</button>
